d1e3aff7f996321eedc7ba13eb54a6def77c82d4
Markus Mayer <widemeadows@gmail.com>
2019-08-06 17:10:07 +0200

    Add driver code

--- Driver.cs
+++ Driver.cs
@@ -11,8 +11,19 @@ namespace Bell
         {
             using (var qsim = new QuantumSimulator())
             {
-                HelloQ.Run(qsim).Wait();
+                // Try initial values
+                var initials = new Result[] { Result.Zero, Result.One };
+                foreach (var initial in initials)
+                {
+                    var res = BellTest.Run(qsim, 1000, initial).Result;
+                    var (numZeros, numOnes) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                }
             }
+
+            System.Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
